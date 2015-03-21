2c0f64446e8b0efb5dd105e4c32c1b595e662391
George Lee <keoki.lee@gmail.com>
2015-03-21 11:39:48 -1000

    Update background and save Github data.
    Updated the background color slightly by stealing a color from
    another project. Wrote an implementation that will save the Github
    data, but will have to test to see if it works.

--- client/assets/js/auth/auth_service.js
+++ client/assets/js/auth/auth_service.js
@@ -4,9 +4,9 @@ module.exports = AuthService;
 
 AuthService.$inject = ['$firebaseAuth'];
 
-function AuthService($firebaseAuth) {
+function AuthService($firebaseAuth, $firebaseObject) {
   let ref = new Firebase('https://til.firebaseio.com/');
-  var auth = $firebaseAuth(ref);
+  let auth = $firebaseAuth(ref);
 
   return {
     isAuthenticated,
@@ -14,10 +14,30 @@ function AuthService($firebaseAuth) {
   };
 
   function isAuthenticated(callback) {
-    auth.$onAuth(callback);
+    auth.$onAuth((authData) =>{
+      // Save the auth data.
+      saveData(authData);
+      callback(authData);
+    });
   }
 
   function loginUser(provider) {
-    return auth.$authWithOAuthPopup(provider);
+    return auth.$authWithOAuthPopup(provider).then(function (authData) {
+      // Persist the user information.
+      saveData(authData);
+      return authData;
+    });
+  }
+
+  function saveData(data) {
+    // Looks like each auth strategy has their own data.
+    if (data.provider === 'github') {
+      let user = $firebaseObject(ref.child('users').child(data.github.email));
+      user.$loaded().then(() => {
+        user.name = data.github.displayName;
+        user.username = data.github.username;
+        user.$save();
+      });
+    }
   }
 }
