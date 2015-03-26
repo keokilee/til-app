f7b78eca7e8cf89e7b0d8529b47781890092cf35
George Lee <keoki.lee@gmail.com>
2015-03-25 19:19:24 -1000

    Cleaning up the logged in view.

--- tasks/copy.js
+++ tasks/copy.js
@@ -27,6 +27,8 @@ gulp.task('copy', function() {
   // Bower css files.
   gulp.src([
     'bower_components/foundation-icon-fonts/foundation-icons.css',
+    'bower_components/foundation-icon-fonts/foundation-icons.woff',
+    'bower_components/foundation-icon-fonts/foundation-icons.ttf',
     'bower_components/foundation/css/foundation.css',
     'bower_components/foundation/css/foundation.css.map'
   ])
